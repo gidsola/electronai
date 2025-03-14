a0d0362e0333213b5a024597e82e5453f1f4c481
Goodsie <gidsola@users.noreply.github.com>
2025-03-14 19:07:12 -0400

    Base for message stamp. More "chat like" disply. Style changes. Code cleanup(more complete). Parsing corrections. TODO: move HTML into index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,47 @@
 
 document.getElementById('inputField').addEventListener('keypress', (event) => {
   const inputField = document.getElementById('inputField');
+
   if (event.key === 'Enter' && !event.shiftKey) {
     event.preventDefault();
     document.getElementById('submitButton').click();
     inputField.value = '';
-  }
+  };
+
 });
 
 document.getElementById('submitButton').addEventListener('click', async () => {
-  const inputField = document.getElementById('inputField');
-  const displayFrame = document.getElementById('displayFrame');
-  const loadingBubble = document.getElementById('loadingBubble');
-  const userInput = inputField.value;
+  const
+    inputField = document.getElementById('inputField'),
+    displayFrame = document.getElementById('displayFrame'),
+    loadingBubble = document.getElementById('loadingBubble'),
+    userInput = inputField.value;
 
   if (!userInput) return;
 
-  loadingBubble.style.display = 'flex';
-
   const
     USERNAME = await window.processBridge.getUsername(),
-    response = await AIRequest(userInput, USERNAME);
+    userDate = new Date().toLocaleString(),
+    userMsg = '<div class="dateTime">' + USERNAME + ' @ ' + userDate + '</div>' + '<div class="message_output"><span>' + marked.parse(userInput) + '</span></div>';
 
+  displayFrame.innerHTML = userMsg;
+
+  loadingBubble.style.display = 'flex';
+  const response = await AIRequest(userInput, USERNAME);
   loadingBubble.style.display = 'none';
 
-  displayFrame.innerHTML = response;
+  displayFrame.innerHTML = marked.parse(response);
+  renderMathInElement(displayFrame);
 
-  renderMathInElement(document.body);
+  const
+    newInner = marked.parse(displayFrame.innerHTML),
+    aiMsg = '<div class="dateTime">AI @ ' + new Date().toLocaleString() + '</div>' + '<div class="message_output">' + newInner + '</div>';
 
-  displayFrame.innerHTML = marked.parse(displayFrame.innerHTML);
-
+  displayFrame.innerHTML = userMsg + aiMsg;
   hljs.highlightAll();
 
 });
 
 async function AIRequest(input, USERNAME) {
   return (await import('./ciDialogExchange.mjs')).ciDialogExchange(input, USERNAME);
-}
-
-
+};
